929c8238bfd170ed101ee65e23cade88a325f2d4
zawmai <zmaitangbau@gmail.com>
2018-04-04 11:57:51 -0700

    Refactored constants import statements and added INVALID_INPUT constant

--- src/configure.py
+++ src/configure.py
@@ -1,12 +1,11 @@
 import configparser
-import credential as c
 import os
 import pathlib
-import constants.pathname as pathname
-import constants.message as message
+from constants.pathname import CONFIG_PATH
+from constants.message import INVALID_INPUT, ENTER_PROMPT
 
 
-def prompt(directive=message.ENTER_PROMPT, target=None, default=None):
+def prompt(directive=ENTER_PROMPT, target=None, default=None):
     response = input(directive.format(target))
     if not response:
         return default
@@ -14,7 +13,7 @@ def prompt(directive=message.ENTER_PROMPT, target=None, default=None):
         return response
 
 
-def write_config_file(filepath=pathname.CONFIG_PATH):
+def write_config_file(filepath=CONFIG_PATH):
     config = configparser.ConfigParser()
 
     default = config['DEFAULT']
@@ -25,9 +24,12 @@ def write_config_file(filepath=pathname.CONFIG_PATH):
 
     config['Credential'] = {}
     cred = config['Credential']
-    cred['client_id'] = prompt(target='client_id', default=c.CLIENT_ID)
-    cred['client_secret'] = prompt(target='client_secret',
-                                   default=c.CLIENT_SECRET)
+    cred['client_id'] = \
+        prompt(target='client_id',
+               default=INVALID_INPUT.format('client_id'))
+    cred['client_secret'] = \
+        prompt(target='client_secret',
+               default=INVALID_INPUT.format('client_secret'))
 
     if os.path.exists(filepath):
         print('Please remove the existing configure file.')
--- src/constants/message.py
+++ src/constants/message.py
@@ -1,3 +1,4 @@
 ENTER_PROMPT = "Please enter {}: "
 AUTH_URL_PROMPT = 'Please go to {} and authorize access'
 AUTH_CODE_PROMPT = 'Enter code: '
+INVALID_INPUT = "<Please enter a valid {}>"
