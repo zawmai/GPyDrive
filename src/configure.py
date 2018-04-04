f07abd0cdcc03d77a5cb2e5de1d37e0062c91f08
zawmai <zmaitangbau@gmail.com>
2018-04-03 18:44:32 -0700

    Removed constants from config file to constants directory

--- src/configure.py
+++ src/configure.py
@@ -2,35 +2,35 @@ import configparser
 import credential as c
 import os
 import pathlib
-DEFAULT_PATH = './../configure.txt'
-DEFAULT_MESSAGE = "Please enter {}: "
+import constants.pathname as pathname
+import constants.message as message
 
 
-def prompt(message=DEFAULT_MESSAGE, target=None, default=None):
-    response = input(message.format(target))
+def prompt(directive=message.ENTER_PROMPT, target=None, default=None):
+    response = input(directive.format(target))
     if not response:
         return default
     else:
         return response
 
 
-config = configparser.ConfigParser()
+def write_config_file(filepath=pathname.CONFIG_PATH):
+    config = configparser.ConfigParser()
 
-default = config['DEFAULT']
-path = pathlib.Path(DEFAULT_PATH)
-default['Path'] = str(path.resolve())
+    default = config['DEFAULT']
+    path = pathlib.Path(filepath)
+    default['config_path'] = str(path.resolve())
+    path = pathlib.Path(filepath)
+    default['token_path'] = str(path.resolve())
 
-config['Credential'] = {}
-cred = config['Credential']
-cred['client_id'] = prompt(target='client_id', default=c.CLIENT_ID)
-cred['client_secret'] = prompt(target='client_secret', default=c.CLIENT_SECRET)
-cred['token_uri'] = "https://accounts.google.com/o/oauth2/token"
-cred['redirect_uri'] = "urn:ietf:wg:oauth:2.0:oob"
-cred['auth_uri'] = "https://accounts.google.com/o/oauth2/v2/auth"
-cred['scope'] = "https://www.googleapis.com/auth/drive"
+    config['Credential'] = {}
+    cred = config['Credential']
+    cred['client_id'] = prompt(target='client_id', default=c.CLIENT_ID)
+    cred['client_secret'] = prompt(target='client_secret',
+                                   default=c.CLIENT_SECRET)
 
-if os.path.exists(DEFAULT_PATH):
-    print('Please remove the existing configure file.')
-else:
-    with open(default['Path'], 'w') as fp:
-        config.write(fp)
+    if os.path.exists(filepath):
+        print('Please remove the existing configure file.')
+    else:
+        with open(filepath, 'w') as fp:
+            config.write(fp)
